3917b40221ffec1facdebdb454e442e7bbbeb8ce
KuoSam <z0935323866@gmail.com>
2024-07-14 21:23:23 +1200

    first

---
+++ index.js
@@ -0,0 +1,105 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+const db = new pg.Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'world',
+  password: 'password',
+  port: 5432
+});
+
+
+db.connect();
+
+async function checkVisisted() {
+
+  const result = await db.query("SELECT * FROM visited_countries");
+
+  let visitedCountries=[];
+
+  for(let i = 0;i<result.rows.length ; i++){
+    visitedCountries.push(result.rows[i].country_code)
+  }
+  console.log(visitedCountries);
+    
+
+  return visitedCountries;
+
+}
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  //Write your code here.
+  const visitedCountries = await checkVisisted();
+
+  res.render('index.ejs',{total: visitedCountries.length, countries:visitedCountries});
+});
+
+app.post("/add", async(req,res)=>{
+  let addcountryName = req.body.country;
+  let addcountryCode;
+
+  try {
+      console.log(addcountryName.toLowerCase());
+
+      const result = await db.query(
+        "SELECT country_code FROM country WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+        [addcountryName.toLowerCase()]
+      );
+
+    addcountryCode = result.rows[0].country_code;
+    try {
+      await db.query("INSERT INTO visited_countries(country_code) VALUES($1)",
+        [addcountryCode]
+      );
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+      const countries = await checkVisisted();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error: "Country has already been added, try again.",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    const countries = await checkVisisted();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: "Country name does not exist, try again.",
+    });
+  }
+
+
+
+
+  // let addcountryCode;
+  // const result = await db.query(`SELECT * FROM country WHERE country_name= $1`,[addcountryName]);
+
+  // if (result.rows.length !== 0){
+  //   // addcountryCode = result.rows[0].country_code;
+
+  //   await db.query("INSERT INTO visited_countries(country_code) VALUES($1)",[addcountryCode],(err,res)=>{
+  //     if(err){
+  //       console.log("Error", err.stack);
+  //     }else{      
+  //       console.log("addcountryCode:"+addcountryCode);      
+  //     }
+  //   });
+  //   res.redirect("/");
+  // }
+
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
